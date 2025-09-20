c56f7ea243c8e03b7849a5e0d369df52e2c96d89
Kvilarinho <katia.vilas@gmail.com>
2025-09-20 14:45:57 +0100

    Implemented CommunicationHandler

---
+++ src/main/java/org/example/CommunicationHandler.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+
+public class CommunicationHandler {
+
+    private static final String BEGINNING_OF_FILE = "BOF";
+    private static final String END_OF_FILE = "EOF";
+
+    private BufferedReader in;
+
+    public String readTask() throws IOException {
+        return in.readLine();
+    }
+
+    public String readCommand() throws IOException {
+        return in.readLine().toUpperCase();
+    }
+
+    public void setIn(BufferedReader in) {
+        this.in = in;
+    }
+
+}
