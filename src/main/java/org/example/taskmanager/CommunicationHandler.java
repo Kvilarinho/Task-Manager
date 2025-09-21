2da8c987c31ef0fafcaf441a646070e127ab620d
Kvilarinho <katia.vilas@gmail.com>
2025-09-21 14:22:55 +0100

    removed unnecessary variables

--- src/main/java/org/example/taskmanager/CommunicationHandler.java
+++ src/main/java/org/example/taskmanager/CommunicationHandler.java
@@ -7,9 +7,6 @@ import java.io.IOException;
 
 public class CommunicationHandler {
 
-    private static final String BEGINNING_OF_FILE = "BOF";
-    private static final String END_OF_FILE = "EOF";
-
     private BufferedReader in;
 
     public String readTask() throws IOException {
